5a9f854c4e6d305c8b993dc9ea53fe95c52c2846
fengjial <jialin_feng@163.com>
2024-03-13 11:41:12 +0800

    update version

--- pymochow/__init__.py
+++ pymochow/__init__.py
@@ -18,7 +18,7 @@ from builtins import bytes
 from . import protocol
 from .client.mochow_client import MochowClient
 
-SDK_VERSION = b'1.0.2'
+SDK_VERSION = b'1.1.1'
 URL_PREFIX = b'/v1'
 DEFAULT_ENCODING = 'UTF-8'
 
