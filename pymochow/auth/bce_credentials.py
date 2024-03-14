5cf5e4c40b3261a8689af68cba06ff5bcb36a2fb
fengjialin <fengjialin@MacBook-Pro.local>
2024-03-14 15:07:23 +0800

    support appbuilder

--- pymochow/auth/bce_credentials.py
+++ pymochow/auth/bce_credentials.py
@@ -19,8 +19,8 @@ from pymochow import compat
 
 class BceCredentials(object):
     """
-    Provides access to the BCE credentials used for accessing BCE services:
-    BCE access key ID and secret access key.
+    Provides access to VDB instance:
+    account and api_key.
     """
     def __init__(self, account, api_key):
         """初始化实例对象
@@ -37,3 +37,27 @@ class BceCredentials(object):
         """
         self.account = compat.convert_to_bytes(account)
         self.api_key = compat.convert_to_bytes(api_key)
+
+
+class AppBuilderCredentials(object):
+    """
+    Provides access to VDB instance from appbuilder:
+    account and api_key and appbuilder_token
+    """
+    def __init__(self, account, api_key, appbuilder_token):
+        """初始化实例对象
+        
+        Args:
+            account (str): 访问用户。
+            api_key (str): 访问密钥。
+            appbuilder_token (str): Appbuilder的token
+        
+        Returns:
+            None
+        
+        Raises:
+            Exception: 无效的访问密钥或密钥密钥。
+        """
+        self.account = compat.convert_to_bytes(account)
+        self.api_key = compat.convert_to_bytes(api_key)
+        self.appbuilder_token = compat.convert_to_bytes(appbuilder_token)
